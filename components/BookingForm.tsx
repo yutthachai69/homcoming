b2aa1c86a74f16e1301243eb96917011e42548db
Yutthachai Khammeephak <Yutthachai@tusm.com>
2026-02-09 11:21:15 +0700

    Fix mobile modal scroll and add upload debug logs

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -82,7 +82,7 @@ export function BookingForm({ open, onOpenChange, table }: BookingFormProps) {
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[425px] max-h-[90vh] overflow-y-auto">
                 <DialogHeader>
                     <DialogTitle>จองโต๊ะ {table.number}</DialogTitle>
                     <DialogDescription>
