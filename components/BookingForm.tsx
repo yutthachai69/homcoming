e6091fa1218bae6ecb70b113f7505487930a74fd
Yutthachai Khammeephak <Yutthachai@tusm.com>
2026-02-09 11:23:08 +0700

    Fix build error (duplicate import) and add upload debug logs

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { useRouter } from "next/navigation"
-import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
