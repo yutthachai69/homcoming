a32baf82c2c8c99cb2474cee25bde8283d9cdd60
Yutthachai Khammeephak <Yutthachai@tusm.com>
2026-02-09 11:16:45 +0700

    Fix upload logging and auto-refresh

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
